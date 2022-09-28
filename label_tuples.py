97cf66142668b56e989557b9d49820e228aeb38e
Noah Da Silva <noah@noahdasilva.com>
2022-09-28 16:52:32 -0400

    Completed label file

--- label_tuples.py
+++ label_tuples.py
@@ -1,3 +1,6 @@
+from email.errors import InvalidMultipartContentTransferEncodingDefect
+
+
 def get_email_list(filename):
     with open("email_lists/labels/"+ filename) as email_list:
         return email_list.read().splitlines()
@@ -11,6 +14,26 @@ cloud = (
     'cloud@email.com',
 ) + tuple(get_email_list('cloud.txt'))
 
+crypto = (
+    'crypto@email.com',
+) + tuple(get_email_list('crypto.txt'))
+
+design = (
+    'design@email.com',
+) + tuple(get_email_list('design.txt'))
+
 development = (
     'development@email.com',
 ) + tuple(get_email_list('development.txt'))
+
+etransfer = (
+    'etransfer@email.com',
+) + tuple(get_email_list('etransfer.txt'))
+
+investing = (
+    'investing@email.com',
+) + tuple(get_email_list('investing.txt'))
+
+security = (
+    'security@email.com',
+) + tuple(get_email_list('security.txt'))
